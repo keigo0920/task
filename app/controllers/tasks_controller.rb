e266498b98afcaafb668c51b7d2ddf48b2d48c47
keigo0920 <keigo-yoshida@dac.co.jp>
2017-06-19 08:29:50 +0000

    for new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,6 @@ class TasksController < ApplicationController
 
   def create
      @task = Task.new(task_params)
-
      if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -29,7 +28,6 @@ class TasksController < ApplicationController
 
   def update
      @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,10 +38,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-     @task = Task.find(params[:id])
-     @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = 'Message は正常に削除されました'
     redirect_to tasks_url
   end
   
@@ -54,6 +51,6 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
 end
