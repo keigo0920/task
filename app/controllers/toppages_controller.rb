da07f87c5e44ceebf66ccb869c3b564a691e19bc
keigo0920 <keigo-yoshida@dac.co.jp>
2017-06-23 01:19:36 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -6,4 +6,15 @@ class ToppagesController < ApplicationController
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
     end
   end
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
 end
